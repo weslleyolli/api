617a1e78fb159f8db4f26de72f4a493d520fe113
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-21 16:01:56 -0300

    PUT

--- index.js
+++ index.js
@@ -4,10 +4,24 @@ const app = express();
 
 app.listen('3000')
 
-app.route('/').get( (req, res) => res.send("hello"))
-app.route('/about').get( (req, res) => res.send("hello everyone"))
+//Post
+//app.route('/').get( (req, res) => res.send("hello"))
+//app.route('/about').get( (req, res) => res.send("hello everyone"))
 
+//POST
 //middleware
+//app.use(express.json())
+
+//app.route('/').post( (req, res) => res.send(req.body))
+
 app.use(express.json())
 
-app.route('/').post( (req, res) => res.send(req.body))
+let author = "Weslley"
+
+//see what you had before 
+app.route('/').get( (req, res) => res.send(author))
+
+app.route('/').put( (req, res) => {
+    author = req.body.author
+    res.send(author)
+})
