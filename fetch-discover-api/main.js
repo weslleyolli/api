319677fb4388df371b23ed7900ee730fb95e857e
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-27 17:02:22 -0300

    fetch-discover-api

---
+++ fetch-discover-api/main.js
@@ -0,0 +1,9 @@
+const url = "http://localhost:5500/api"
+
+function getUsers() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data =>  renderApiResult.textContent = JSON.stringify(data))
+        .catch(err => console.log(err))
+}
+getUsers()
