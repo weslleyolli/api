2767c95da25d4864763b9094350ab216b6804a92
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-27 19:21:42 -0300

    GET with params

--- fetch-discover-api/main.js
+++ fetch-discover-api/main.js
@@ -6,4 +6,16 @@ function getUsers() {
         .then(data =>  renderApiResult.textContent = JSON.stringify(data))
         .catch(err => console.log(err))
 }
-getUsers()
+
+ function getUser() {
+    fetch(`${url}/1`)
+        .then(response => response.json())
+        .then(data =>  {
+            userName.textContent = data.name
+            userCity.textContent = data.city
+            userAvatar.src = data.avatar
+        })
+        .catch(err => console.log(err))
+ }
+ getUser()
+ getUsers()
