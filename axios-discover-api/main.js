bf60b3a347c03f6b7d2d419254262fc31eea8fff
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-28 22:50:59 -0300

    post with axios

--- axios-discover-api/main.js
+++ axios-discover-api/main.js
@@ -1,3 +1,5 @@
+
+
 const url = "http://localhost:5500/api"
 
 function getUser() {
@@ -8,4 +10,20 @@ function getUser() {
         .catch(error => console.error(error))
 }
 
+function addNewUser(newUser) {
+    axios.post(url, )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => console.error(error))
+}
+
 getUser()
+
+const newUser = {
+    name: 'Weslley Olli',
+    avatar: "https://picsum.photos/200/300",
+    city: "Campina grande"
+}
+
+addNewUser(newUser)
